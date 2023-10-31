f0f5a7b7e0c4920092070dc44f4643a15e78c20c
Nguyen Tien Dan <nguyen.tien.dan@sun-asterisk.com>
2023-10-31 13:55:39 +0700

    Done section 5 REST API Security

--- src/main/java/com/luv2code/springboot/cruddemo/sercurity/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springboot/cruddemo/sercurity/DemoSecurityConfig.java
@@ -8,21 +8,34 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
+    // add support for JDBC .... no more hardcoded users
+
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
 
-        UserDetails john = User.builder().username("john").password("{noop}123456").roles("EMPLOYEE").build();
-        UserDetails mary = User.builder().username("mary").password("{noop}123456").roles("MANAGER").build();
-        UserDetails susan = User.builder().username("susan").password("{noop}123456").roles("MANAGER", "ADMIN").build();
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
 
-        return new InMemoryUserDetailsManager(john, mary, susan);
+        // define query to retrieve a user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id, pw, active from members where user_id=?"
+        );
+
+        // define query to retrieve the authorities/roles by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id, role from roles where user_id=?"
+        );
+
+        return jdbcUserDetailsManager;
     }
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 
@@ -42,4 +55,14 @@ public class DemoSecurityConfig {
 
         return httpSecurity.build();
     }
+
+    //    @Bean
+    //    public InMemoryUserDetailsManager userDetailsManager() {
+    //
+    //        UserDetails john = User.builder().username("john").password("{noop}123456").roles("EMPLOYEE").build();
+    //        UserDetails mary = User.builder().username("mary").password("{noop}123456").roles("MANAGER").build();
+    //        UserDetails susan = User.builder().username("susan").password("{noop}123456").roles("MANAGER", "ADMIN").build();
+    //
+    //        return new InMemoryUserDetailsManager(john, mary, susan);
+    //    }
 }
